94107e0dc70bd4b98ba665725124d1f3b6954147
yuliyago <yuuliya02@proton.me>
2023-08-04 14:24:44 +0300

    Lambda task was done.

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            List<Integer> dividedNumbers = new ArrayList<>();
+            for (Integer number : x) {
+                dividedNumbers.add(number / divider);
+            }
+            return dividedNumbers;
+        };
     }
 }
